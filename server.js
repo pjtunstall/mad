a333130fb82368e3f912ad5800d710d875c548ca
pjunstall <pjtunstall@gmail.com>
2024-07-20 12:13:32 +0100

    log that server could be listening on localhost or its own IP address

--- server.js
+++ server.js
@@ -95,7 +95,9 @@ disconnectAll();
 
 const port = process.env.PORT || 3000;
 server.listen(port, () =>
-  console.log(`Server listening on port http://${SERVER_IP_ADDRESS}:3000`)
+  console.log(
+    `Server listening on http://localhost:3000 or http://${SERVER_IP_ADDRESS}:3000`
+  )
 );
 
 function disconnectAll() {
